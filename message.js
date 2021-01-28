d64e0f396c552a2da99d79b7a3280ceb19f28d72
BalqeesA <basbosared@ymail.com>
2021-01-28 15:20:55 +0200

    update \

---
+++ message.js
@@ -0,0 +1,14 @@
+console.log('hello our visitor ');
+
+
+var age = prompt('what is your age ?');
+console.log('age');
+if (age < 20 )
+{message = 'cool';}
+else if (age < 30 )
+{message = 'Amazing';}
+else if ( age <= 40 ){
+message = 'welcome handsome';}
+else{
+message = 'welcome';}
+document.write('<h4>' + message +'</h4>');
